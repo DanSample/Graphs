4c3d639b2cda82b13e65a371950764046b446b4e
Dan Sample <danssample@gmail.com>
2020-07-29 19:33:24 -0700

    finished day 3

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,18 @@
+import random
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -28,25 +43,47 @@ class SocialGraph:
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
 
+    def fisher_yates_shuffle(self, l):
+        for i in range(0, len(l)):
+            random_index = random.randint(i, len(l) - 1)
+            l[random_index], l[i] = l[i], l[random_index]
+
+
     def populate_graph(self, num_users, avg_friendships):
-        """
+            """
         Takes a number of users and an average number of friendships
         as arguments
-
+​
         Creates that number of users and a randomly distributed friendships
         between those users.
-
+​
         The number of users must be greater than the average number of friendships.
         """
-        # Reset graph
-        self.last_id = 0
-        self.users = {}
-        self.friendships = {}
-        # !!!! IMPLEMENT ME
+            # Reset graph
+            self.last_id = 0
+            self.users = {}
+            self.friendships = {}
+            # !!!! IMPLEMENT ME    ​
+            # Add users
+            for user in range(num_users):
+                self.add_user(user)
+            # starts at 1, up to and including num_users   ​   ​
+            # * Hint 1: To create N random friendships, 
+            # you could create a list with all possible friendship combinations of user ids,    ​
+            friendship_combinations = []
+            # O(n^2)
+            for user in range(1, self.last_id + 1):
+                for friend in range(user + 1, self.last_id + 1):
+                    friendship_combinations.append((user, friend))
+            # shuffle the list
+            self.fisher_yates_shuffle(friendship_combinations)
+            # then grab the first N elements from the list. 
+            total_friendships = num_users * avg_friendships
+            friends_to_make = friendship_combinations[:(total_friendships // 2)]
+            # Create friendships
+            for friendship in friends_to_make:
+                self.add_friendship(friendship[0], friendship[1])
 
-        # Add users
-
-        # Create friendships
 
     def get_all_social_paths(self, user_id):
         """
@@ -58,7 +95,24 @@ class SocialGraph:
         The key is the friend's ID and the value is the path.
         """
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+        
+        path = [user_id]
+        q = Queue()
+        q.enqueue(path)
+
+        while q.size():
+            current_path = q.dequeue()
+
+            current_vertex = current_path[-1]
+
+            if current_vertex not in visited:
+                visited[current_vertex] = current_path
+                neighbors = self.friendships[current_vertex]
+                for neighbor in neighbors:
+                    neighbor_path = current_path[:]
+                    neighbor_path.append(neighbor)
+                    q.enqueue(neighbor_path)
+
         return visited
 
 
